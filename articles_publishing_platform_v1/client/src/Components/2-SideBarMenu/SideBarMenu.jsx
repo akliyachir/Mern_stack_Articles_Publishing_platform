eb4d0f375daeb3fc1a917bbe50ca438044bb5eea
akliyachir <akli.yachir@gmail.com>
2023-01-29 18:50:52 -0300

    mern stack with user auth, article publishing platform build

--- articles_publishing_platform_v1/client/src/Components/2-SideBarMenu/SideBarMenu.jsx
+++ articles_publishing_platform_v1/client/src/Components/2-SideBarMenu/SideBarMenu.jsx
@@ -1,31 +1,28 @@
 import './SideBarMenu.css';
 import { NavLink } from 'react-router-dom';
-import { menuItemsList } from './SideBarMenuItems';
 import { FaRegWindowClose } from 'react-icons/fa';
+import { menuItemsList } from '../../listsAndReusedConsts/SideBarMenuItems.js';
 
 export default function SideBarMenu({ isMenuOpen, closeSideMenu }) {
-  return (
-    <div className={isMenuOpen ? 'SideBarMenuOpen' : 'SideBarMenuClosed'}>
-      <div className='SideBarMenuContent'>
-        <div className='closeSideBarCross' onClick={closeSideMenu}>
-          <FaRegWindowClose />
-        </div>
-        <div className='menuItem'>
-          {menuItemsList.map((item) => {
-            const { menu_item_id, title, url } = item;
-            return (
-              <div key={menu_item_id}>
-                <span
-                  className='NavLinkMenuItemStyling'
-                  onClick={closeSideMenu}
-                >
-                  <NavLink to={url}>{title}</NavLink>
-                </span>
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={isMenuOpen ? 'SideBarMenuOpen' : 'SideBarMenuClosed'}>
+			<div className='SideBarMenuContent'>
+				<div className='closeSideBarCross' onClick={closeSideMenu}>
+					<FaRegWindowClose />
+				</div>
+				<div className='menuItem'>
+					{menuItemsList.map((item) => {
+						const { menu_item_id, title, url } = item;
+						return (
+							<div key={menu_item_id}>
+								<span className='NavLinkMenuItemStyling' onClick={closeSideMenu}>
+									<NavLink to={url}>{title}</NavLink>
+								</span>
+							</div>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 }
