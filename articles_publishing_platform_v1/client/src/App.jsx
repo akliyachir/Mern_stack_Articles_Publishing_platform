d539ea24e16c2439117ea423862bd4075c20f0cf
akliyachir <akli.yachir@gmail.com>
2023-01-30 01:27:08 -0300

    mern stack, articles publishing platform, with user auth, building

--- articles_publishing_platform_v1/client/src/App.jsx
+++ articles_publishing_platform_v1/client/src/App.jsx
@@ -1,8 +1,8 @@
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
 } from 'react-router-dom';
 // -- pages import
 import RootLayout from './Pages/1-RootLayout/RootLayout';
@@ -11,29 +11,35 @@ import SignUpContainer from './Pages/4-SignUp/SignUpContainer';
 import SignInContainer from './Pages/3-SignIn/SignInContainer';
 // -- contexts
 import UserContextProvider from './Contexts/UserContext';
-
+import CreateArticle from './Pages/UserPages/1-CreateArticle/CreateArticle';
+// -- user routes import
 //-- create router
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path='/' element={<RootLayout />} errorElement={<ErrorPage />}>
-			<Route
-				path='/signin'
-				element={<SignInContainer />}
-				errorElement={<ErrorPage />}
-			/>
-			<Route
-				path='/signup'
-				element={<SignUpContainer />}
-				errorElement={<ErrorPage />}
-			/>
-		</Route>
-	)
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />} errorElement={<ErrorPage />}>
+      <Route
+        path='/signin'
+        element={<SignInContainer />}
+        errorElement={<ErrorPage />}
+      />
+      <Route
+        path='/signup'
+        element={<SignUpContainer />}
+        errorElement={<ErrorPage />}
+      />
+      <Route
+        path='/create_article'
+        element={<CreateArticle />}
+        errorElement={<ErrorPage />}
+      />
+    </Route>
+  )
 );
 
 export default function App() {
-	return (
-		<UserContextProvider>
-			<RouterProvider router={router} />
-		</UserContextProvider>
-	);
+  return (
+    <UserContextProvider>
+      <RouterProvider router={router} />
+    </UserContextProvider>
+  );
 }
---
+++ articles_publishing_platform_v1/client/src/Pages/UserPages/1-CreateArticle/CreateArticle.jsx
@@ -0,0 +1,8 @@
+import './CreateArticle.css';
+export default function CreateArticle() {
+  return (
+    <div className='CreateArticle'>
+      <div className='CreateArticleContent'>CreateArticle</div>
+    </div>
+  );
+}
