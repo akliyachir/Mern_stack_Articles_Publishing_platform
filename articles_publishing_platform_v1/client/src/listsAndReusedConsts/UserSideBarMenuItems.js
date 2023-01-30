783f57725d09afbba3770fd2e1a8775c7c649445
akliyachir <akli.yachir@gmail.com>
2023-01-29 22:35:01 -0300

    mern stack with user auth, article publishing platform build, currently making the user menu

--- articles_publishing_platform_v1/client/src/listsAndReusedConsts/UserSideBarMenuItems.js
+++ articles_publishing_platform_v1/client/src/listsAndReusedConsts/UserSideBarMenuItems.js
@@ -1,27 +1,32 @@
 export const userMenuItemsList = [
 	{
 		menu_item_id: 1,
-		title: 'about',
-		url: '/about',
+		title: 'create an article',
+		url: '/create_article',
 	},
 	{
 		menu_item_id: 2,
-		title: 'sign in',
-		url: '/signin',
+		title: 'my articles',
+		url: '/my_articles',
 	},
 	{
 		menu_item_id: 3,
-		title: 'create an acount',
-		url: '/signup',
+		title: 'profile',
+		url: '/profile',
 	},
 	{
 		menu_item_id: 4,
-		title: 'services',
-		url: '/services',
+		title: 'explore',
+		url: '/',
 	},
 	{
 		menu_item_id: 5,
-		title: 'contact',
-		url: '/contact',
+		title: 'talk with us',
+		url: '/talk_with_us',
+	},
+	{
+		menu_item_id: 6,
+		title: 'disconnect',
+		url: '/disconnect',
 	},
 ];
