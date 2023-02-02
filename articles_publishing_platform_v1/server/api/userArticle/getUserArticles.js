73c874e555035b6a9b8a34cbd423b88ff9067153
akliyachir <akli.yachir@gmail.com>
2023-02-02 01:42:11 -0300

    mern stack, articles publishing platform building

--- articles_publishing_platform_v1/server/api/userArticle/getUserArticles.js
+++ articles_publishing_platform_v1/server/api/userArticle/getUserArticles.js
@@ -1,3 +1,7 @@
+import Article from '../../models/userArticleModel.js';
+import User from '../../models/userModel.js';
+import jsonwebtoken from 'jsonwebtoken';
+
 const getUserArticles = async (req, res) => {};
 
 export { getUserArticles };
