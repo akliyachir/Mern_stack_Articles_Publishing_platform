d0c87e0c69ed803ad8b9487313bfa43b3892e452
akliyachir <akli.yachir@gmail.com>
2023-02-02 05:58:35 -0300

    mern stack, articles publishing platform building

--- articles_publishing_platform_v1/client/src/UserPages/2-GetUserArticles/GetUserArticles.jsx
+++ articles_publishing_platform_v1/client/src/UserPages/2-GetUserArticles/GetUserArticles.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function GetUserArticles() {
-	return <div className='GetUserArticles'>GetUserArticles</div>;
+	return (
+		<div className='GetUserArticles'>
+			<div className='GetUserArticlesContainer'>
+				ceci et cela, les articles a disposition
+			</div>
+		</div>
+	);
 }
