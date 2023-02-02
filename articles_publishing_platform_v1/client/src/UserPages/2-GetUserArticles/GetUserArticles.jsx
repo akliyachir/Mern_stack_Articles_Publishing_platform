fa0fb9100ad7034b39e497e4f06e1361ce343129
akliyachir <akli.yachir@gmail.com>
2023-02-02 06:33:13 -0300

    mern stack, articles publishing platform building

--- articles_publishing_platform_v1/client/src/UserPages/2-GetUserArticles/GetUserArticles.jsx
+++ articles_publishing_platform_v1/client/src/UserPages/2-GetUserArticles/GetUserArticles.jsx
@@ -1,6 +1,34 @@
-import React from 'react';
+import './GetUserArticles.css';
+import { useState, useEffect } from 'react';
+import backendUrl from '../../listsAndReusedConsts/backendUrl';
 
 export default function GetUserArticles() {
+	const [artilce, setArtilce] = useState([]);
+
+	useEffect(() => {
+		const getArticles = async () => {
+			const { token } = globalThis.localStorage('user');
+			const response = await fetch(backendUrl + 'user_article', {
+				headers: {
+					authorization: JSON.stringify(`Bearer ${token}`),
+				},
+			});
+
+			const result = await response.json();
+
+			if (!response.ok) {
+				console.log('not ok');
+				console.error(result.message);
+			}
+
+			if (!response.ok) {
+				console.log('ok');
+				setArtilce(result.message);
+				console.log(result.message);
+			}
+		};
+	}, []);
+
 	return (
 		<div className='GetUserArticles'>
 			<div className='GetUserArticlesContainer'>
