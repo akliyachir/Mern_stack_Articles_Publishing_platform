44273be7255ef22ceba176679c2651e3196cb01e
akliyachir <akli.yachir@gmail.com>
2023-02-03 00:56:07 -0300

    mern stack app, article publishing platform build

--- articles_publishing_platform_v1/client/src/UserPages/1-CreateArticle/CreateArticle.jsx
+++ articles_publishing_platform_v1/client/src/UserPages/1-CreateArticle/CreateArticle.jsx
@@ -6,110 +6,112 @@ import TextAreaFormTemplate from '../../UserComponents/2-TextAreaFormTemplate/Te
 import backendUrl from '../../listsAndReusedConsts/backendUrl';
 
 export default function CreateArticle() {
-	// -- form data useState
-	const [createArticleFormData, setCreateArticleFormData] = useState({
-		article_title: '',
-		article_image_url: '',
-		article_body: '',
-	});
-	const { article_title, article_image_url, article_body } =
-		createArticleFormData;
-	//-- auto handle Input On Change
-	const handleInputOnChange = (e) => {
-		setCreateArticleFormData({
-			...createArticleFormData,
-			[e.target.name]: e.target.value,
-		});
-	};
+  // -- form data useState
+  const [createArticleFormData, setCreateArticleFormData] = useState({
+    article_title: '',
+    article_image_url: '',
+    article_body: '',
+    article_id: crypto.randomUUID(),
+  });
+  const { article_title, article_image_url, article_body } =
+    createArticleFormData;
+  //-- auto handle Input On Change
+  const handleInputOnChange = (e) => {
+    setCreateArticleFormData({
+      ...createArticleFormData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-	//-- create response area
-	const [serverResponse, setServerResponse] = useState('');
-	// -- and redirect in case of success
-	const navigate = useNavigate();
+  //-- create response area
+  const [serverResponse, setServerResponse] = useState('');
+  // -- and redirect in case of success
+  const navigate = useNavigate();
 
-	//-- handle submitNewArticleData
-	const handleOnSubmitCreateNewArticle = async (e) => {
-		e.preventDefault();
-		// -- get token from localStorage
-		const { token } = JSON.parse(globalThis.localStorage.getItem('user'));
+  //-- handle submitNewArticleData
+  const handleOnSubmitCreateNewArticle = async (e) => {
+    e.preventDefault();
+    // -- get token from localStorage
+    const { token } = JSON.parse(globalThis.localStorage.getItem('user'));
 
-		const response = await fetch(backendUrl + 'user_article', {
-			method: 'POST',
-			headers: {
-				'Content-type': 'application/json',
-				authorization: JSON.stringify(`Bearer ${token}`),
-			},
-			body: JSON.stringify(createArticleFormData),
-		});
+    const response = await fetch(backendUrl + 'user_article', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        authorization: JSON.stringify(`Bearer ${token}`),
+      },
+      body: JSON.stringify(createArticleFormData),
+    });
 
-		const result = await response.json();
+    const result = await response.json();
 
-		if (!response.ok) {
-			console.log('not ok');
-			setServerResponse(result.message);
-			setTimeout(() => {
-				setServerResponse('');
-			}, 3000);
-		}
+    if (!response.ok) {
+      console.log('not ok');
+      setServerResponse(result.message);
+      setTimeout(() => {
+        setServerResponse('');
+      }, 3000);
+    }
 
-		if (response.ok) {
-			console.log('ok');
-			setServerResponse(result.message);
-			setTimeout(() => {
-				setCreateArticleFormData({
-					article_title: '',
-					article_image_url: '',
-					article_body: '',
-				});
-				navigate('/user_articles');
-			}, 3000);
-		}
-	};
+    if (response.ok) {
+      console.log('ok');
+      setServerResponse(result.message);
+      setTimeout(() => {
+        setCreateArticleFormData({
+          article_title: '',
+          article_image_url: '',
+          article_body: '',
+          article_id: crypto.randomUUID(),
+        });
+        navigate('/user_articles');
+      }, 3000);
+    }
+  };
 
-	//-- return jsx
-	return (
-		<div className='CreateArticle'>
-			<div className='CreateArticleContent'>
-				<form
-					onSubmit={handleOnSubmitCreateNewArticle}
-					className='formCreateArticle'
-				>
-					<div className='createArticlePageName'>
-						{serverResponse ? serverResponse : <h1>Create an article</h1>}
-					</div>
+  //-- return jsx
+  return (
+    <div className='CreateArticle'>
+      <div className='CreateArticleContent'>
+        <form
+          onSubmit={handleOnSubmitCreateNewArticle}
+          className='formCreateArticle'
+        >
+          <div className='createArticlePageName'>
+            {serverResponse ? serverResponse : <h1>Create an article</h1>}
+          </div>
 
-					<InputFormTemplate
-						label='Title'
-						type='text'
-						name='article_title'
-						value={article_title}
-						handleInputOnChange={handleInputOnChange}
-					/>
-					<InputFormTemplate
-						label='Image link'
-						type='text'
-						name='article_image_url'
-						value={article_image_url}
-						handleInputOnChange={handleInputOnChange}
-					/>
-					<TextAreaFormTemplate
-						label='body'
-						type='textarea'
-						name='article_body'
-						value={article_body}
-						handleInputOnChange={handleInputOnChange}
-					/>
-					<div className='buttonOnSubmitCreateNewArticleContainer'>
-						<button
-							type='submit'
-							className='buttonOnSubmitCreateNewArticle'
-							onClick={handleOnSubmitCreateNewArticle}
-						>
-							Submit
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	);
+          <InputFormTemplate
+            label='Title'
+            type='text'
+            name='article_title'
+            value={article_title}
+            handleInputOnChange={handleInputOnChange}
+          />
+          <InputFormTemplate
+            label='Image link'
+            type='text'
+            name='article_image_url'
+            value={article_image_url}
+            handleInputOnChange={handleInputOnChange}
+          />
+          <TextAreaFormTemplate
+            label='body'
+            type='textarea'
+            name='article_body'
+            value={article_body}
+            handleInputOnChange={handleInputOnChange}
+          />
+          <div className='buttonOnSubmitCreateNewArticleContainer'>
+            <button
+              type='submit'
+              className='buttonOnSubmitCreateNewArticle'
+              onClick={handleOnSubmitCreateNewArticle}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
