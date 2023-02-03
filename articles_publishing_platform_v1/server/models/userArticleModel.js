b51bfb83a77164c885f47bd1f0effb53ac5056f6
akliyachir <akli.yachir@gmail.com>
2023-02-03 00:41:48 -0300

    mern stack app, article publishing platform build

--- articles_publishing_platform_v1/server/models/userArticleModel.js
+++ articles_publishing_platform_v1/server/models/userArticleModel.js
@@ -1,29 +1,32 @@
 import mongoose, { Schema } from 'mongoose';
 
 const articleSchema = new Schema({
-	article_title: {
-		type: String,
-		required: true,
-	},
-	article_image_url: {
-		type: String,
-	},
-	article_body: {
-		type: String,
-		required: true,
-	},
-	article_creation_date: {
-		type: Date,
-		default: Date.now,
-	},
+  article_title: {
+    type: String,
+    required: true,
+  },
+  article_image_url: {
+    type: String,
+  },
+  article_body: {
+    type: String,
+    required: true,
+  },
+  article_creation_date: {
+    type: Date,
+    default: Date.now,
+  },
 
-	article_last_update: {
-		type: Date,
-	},
-
-	user_id: {
-		type: String,
-	},
+  article_last_update: {
+    type: Date,
+  },
+  article_id: {
+    type: String,
+    required: true,
+  },
+  user_id: {
+    type: String,
+  },
 });
 
 const Article = mongoose.model('Article', articleSchema);
