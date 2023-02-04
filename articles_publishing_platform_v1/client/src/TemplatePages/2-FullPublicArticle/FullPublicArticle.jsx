c2ed06a76899b51ef96bf1e671e1cacf683abec3
akliyachir <akli.yachir@gmail.com>
2023-02-04 12:52:01 -0300

    mern stack, article publishing platform build, now making display all public articles at home page

--- articles_publishing_platform_v1/client/src/TemplatePages/2-FullPublicArticle/FullPublicArticle.jsx
+++ articles_publishing_platform_v1/client/src/TemplatePages/2-FullPublicArticle/FullPublicArticle.jsx
@@ -44,12 +44,21 @@ export default function FullPublicArticle() {
 
 	return (
 		<article className='FullPublicArticle'>
-			<div className='FullPublicArticleContent'></div>
+			<div className='FullPublicArticleContent'>
+				<div
+					style={{ backgroundImage: `url(${article_image_url})` }}
+					className='article_image_url'
+				></div>
+				<div className='article_title'>{article_title}</div>
+				<div className='article_body'>{article_body}</div>
+				<div className='article_creation_date'>
+					{article_creation_date.toLocaleString().slice(0, 10)}
+				</div>
+			</div>
 		</article>
 	);
 }
 /* 
-article_body,
 article_creation_date,
 article_id,
 article_image_url,
