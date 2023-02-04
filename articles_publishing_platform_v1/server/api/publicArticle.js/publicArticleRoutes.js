a3df649c996c17b51bf0bce3c5b5778651be612f
akliyachir <akli.yachir@gmail.com>
2023-02-04 12:02:33 -0300

    mern stack, article publishing platform build, now making display all public articles at home page

--- articles_publishing_platform_v1/server/api/publicArticle.js/publicArticleRoutes.js
+++ articles_publishing_platform_v1/server/api/publicArticle.js/publicArticleRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import showAllPublicArticles from './showAllPublicArticlesController.js';
+import showOnlyOnePublicArticles from './showOnlyOnePublicArticlesController.js';
 
 const router = Router();
 
 router.get('/', showAllPublicArticles);
-router.get('/', showOnlyOnePublicArticles);
+router.get('/:public_article_id', showOnlyOnePublicArticles);
 
 export default router;
---
+++ articles_publishing_platform_v1/server/api/publicArticle.js/showOnlyOnePublicArticlesController.js
@@ -0,0 +1,28 @@
+import Article from '../../models/userArticleModel.js';
+
+export default async function showOnlyOnePublicArticles(req, res) {
+	const { public_article_id } = req.params;
+
+	try {
+		const articles = await Article.findOne({
+			article_is_public: true,
+			article_id: public_article_id,
+		})
+			.sort({
+				article_creation_date: -1,
+			})
+			.select({
+				article_is_public: 0,
+				article_user_publisher: 0,
+				article_last_update: 0,
+				user_id: 0,
+				_id: 0,
+				__v: 0,
+			});
+		res.status(200).json({ message: articles });
+		return;
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+		return;
+	}
+}
