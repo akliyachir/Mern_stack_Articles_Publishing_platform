f259ac61feac5b088f417abefd15929d985ae09b
akliyachir <akli.yachir@gmail.com>
2023-02-04 11:42:11 -0300

    mern stack, article publishing platform build, now making display all public articles at home page

--- articles_publishing_platform_v1/client/src/TemplatePages/1-PublicArticleHomeCard/PublicArticleHomeCard.jsx
+++ articles_publishing_platform_v1/client/src/TemplatePages/1-PublicArticleHomeCard/PublicArticleHomeCard.jsx
@@ -11,28 +11,32 @@ export default function PublicArticleHomeCard({ item }) {
 	} = item;
 
 	return (
-		<div key={article_id}>
-			<article key={article_id} className='UserArticleCard'>
-				<NavLink to={'/article/' + article_id}>
-					<div className='UserArticleCardContent'>
-						<div
-							style={{ backgroundImage: `url(${article_image_url})` }}
-							className='article_image_url_landing_page'
-						></div>
-						<div className='article_title_landing_page'>
-							{article_title.slice(0, 52)}
-							{article_title.length >= 52 && '...'}
-						</div>
-						<p className='article_body_landing_page_landing_page'>
-							{article_body.slice(0, 173)}
-							{article_body.length >= 173 && '...'}
-						</p>
-						<p className='article_creation_date_landing_page'>
-							{article_creation_date.toLocaleString().slice(0, 10)}
-						</p>
+		<article className='PublicArticleHomeCard'>
+			<NavLink to={'/article/' + article_id}>
+				<div className='PublicArticleHomeCardContent'>
+					<div key={article_id}>
+						<article key={article_id} className='UserArticleCard'>
+							<div className='UserArticleCardContent'>
+								<div
+									style={{ backgroundImage: `url(${article_image_url})` }}
+									className='article_image_url'
+								></div>
+								<div className='article_title'>
+									{article_title.slice(0, 52)}
+									{article_title.length >= 52 && '...'}
+								</div>
+								<p className='article_body'>
+									{article_body.slice(0, 173)}
+									{article_body.length >= 173 && '...'}
+								</p>
+								<p className='article_creation_date'>
+									{article_creation_date.toLocaleString().slice(0, 10)}
+								</p>
+							</div>
+						</article>
 					</div>
-				</NavLink>
-			</article>
-		</div>
+				</div>
+			</NavLink>
+		</article>
 	);
 }
