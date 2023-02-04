c5ad519bdf68d279ef512a013e2f5359561e1a5e
akliyachir <akli.yachir@gmail.com>
2023-02-04 09:15:36 -0300

    mern stack, article publishing platform build, now making display all public articles at home page

--- articles_publishing_platform_v1/client/src/Pages/5-ShowAllPublicArticles/ShowAllPublicArticles.jsx
+++ articles_publishing_platform_v1/client/src/Pages/5-ShowAllPublicArticles/ShowAllPublicArticles.jsx
@@ -1,6 +1,21 @@
 import './ShowAllPublicArticles.css';
+import backendUrl from '../../listsAndReusedConsts/backendUrl';
+import { useEffect } from 'react';
 
 export default function ShowAllPublicArticles() {
+	useEffect(() => {
+		const fetchPublicArticles = async () => {
+			const response = await fetch(`${backendUrl}articles`);
+			const result = await response.json();
+			console.log(result.message);
+		};
+
+		console.log(backendUrl);
+
+		fetchPublicArticles();
+	}, []);
+
+	//-- return jsx
 	return (
 		<div className='ShowAllPublicArticles'>
 			<div className='ShowAllPublicArticlesContent'>
