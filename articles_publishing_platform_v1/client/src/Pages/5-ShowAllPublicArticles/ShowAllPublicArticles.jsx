4a0b4b9c5492f46ed3db9d8bfbca9888493f951f
akliyachir <akli.yachir@gmail.com>
2023-02-04 08:24:19 -0300

    mern stack, article publishing platform build, now making display all public articles at home page

---
+++ articles_publishing_platform_v1/client/src/Pages/5-ShowAllPublicArticles/ShowAllPublicArticles.jsx
@@ -0,0 +1,11 @@
+import './ShowAllPublicArticles.css';
+
+import React from 'react';
+
+export default function ShowAllPublicArticles() {
+	return (
+		<div className='ShowAllPublicArticles'>
+			<div className='ShowAllPublicArticlesContent'>ShowAllPublicArticles</div>
+		</div>
+	);
+}
