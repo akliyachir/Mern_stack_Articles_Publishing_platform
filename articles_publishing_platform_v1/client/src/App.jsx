a89a551ab001491a60d3713ce5874a168923214e
akliyachir <akli.yachir@gmail.com>
2023-02-06 01:14:42 -0300

    article publishing platform build, currently adding user disconnect from the account mern stack

--- articles_publishing_platform_v1/client/src/App.jsx
+++ articles_publishing_platform_v1/client/src/App.jsx
@@ -1,8 +1,8 @@
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
 } from 'react-router-dom';
 // -- pages import
 import RootLayout from './Pages/1-RootLayout/RootLayout';
@@ -17,61 +17,67 @@ import TestComponent from './TestComponent/TestComponent';
 import GetUserFullArticleTemplate from './UserPages/3-GetUserFullArticleTemplate/GetUserFullArticleTemplate';
 import ShowAllPublicArticles from './Pages/5-ShowAllPublicArticles/ShowAllPublicArticles';
 import FullPublicArticle from './TemplatePages/2-FullPublicArticle/FullPublicArticle';
+import UserDisconnect from './UserPages/0-UserDisconnect/UserDisconnect';
 
 // -- user routes import
 //-- create router
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path='/' element={<RootLayout />} errorElement={<ErrorPage />}>
-			<Route
-				path='/'
-				element={<ShowAllPublicArticles />}
-				errorElement={<ErrorPage />}
-			/>
-			<Route
-				path='/article/:public_article_id'
-				element={<FullPublicArticle />}
-				errorElement={<ErrorPage />}
-			/>
-			<Route
-				path='/signin'
-				element={<SignInContainer />}
-				errorElement={<ErrorPage />}
-			/>
-			<Route
-				path='/signup'
-				element={<SignUpContainer />}
-				errorElement={<ErrorPage />}
-			/>
-			<Route
-				path='/create_article'
-				element={<CreateArticle />}
-				errorElement={<ErrorPage />}
-			/>
-			<Route
-				path='/user_articles'
-				element={<GetUserArticles />}
-				errorElement={<ErrorPage />}
-			/>
-			<Route
-				path='/user_article/:user_article_id'
-				element={<GetUserFullArticleTemplate />}
-				errorElement={<ErrorPage />}
-			/>
+  createRoutesFromElements(
+    <Route path='/' element={<RootLayout />} errorElement={<ErrorPage />}>
+      <Route
+        path='/'
+        element={<ShowAllPublicArticles />}
+        errorElement={<ErrorPage />}
+      />
+      <Route
+        path='/article/:public_article_id'
+        element={<FullPublicArticle />}
+        errorElement={<ErrorPage />}
+      />
+      <Route
+        path='/signin'
+        element={<SignInContainer />}
+        errorElement={<ErrorPage />}
+      />
+      <Route
+        path='/signup'
+        element={<SignUpContainer />}
+        errorElement={<ErrorPage />}
+      />
+      <Route
+        path='/disconnect'
+        element={<UserDisconnect />}
+        errorElement={<ErrorPage />}
+      />
+      <Route
+        path='/create_article'
+        element={<CreateArticle />}
+        errorElement={<ErrorPage />}
+      />
+      <Route
+        path='/user_articles'
+        element={<GetUserArticles />}
+        errorElement={<ErrorPage />}
+      />
+      <Route
+        path='/user_article/:user_article_id'
+        element={<GetUserFullArticleTemplate />}
+        errorElement={<ErrorPage />}
+      />
 
-			<Route
-				path='/tomato'
-				element={<TestComponent />}
-				errorElement={<ErrorPage />}
-			/>
-		</Route>
-	)
+      <Route
+        path='/tomato'
+        element={<TestComponent />}
+        errorElement={<ErrorPage />}
+      />
+    </Route>
+  )
 );
 
 export default function App() {
-	return (
-		<UserContextProvider>
-			<RouterProvider router={router} />
-		</UserContextProvider>
-	);
+  return (
+    <UserContextProvider>
+      <RouterProvider router={router} />
+    </UserContextProvider>
+  );
 }
---
+++ articles_publishing_platform_v1/client/src/UserPages/0-UserDisconnect/UserDisconnect.jsx
@@ -0,0 +1,11 @@
+import './UserDisconnect.css';
+
+export default function UserDisconnect() {
+  return (
+    <div className='UserDisconnect'>
+      <div className='UserDisconnectContent'>
+        <div>Hope to see you soon ! </div>
+      </div>
+    </div>
+  );
+}
