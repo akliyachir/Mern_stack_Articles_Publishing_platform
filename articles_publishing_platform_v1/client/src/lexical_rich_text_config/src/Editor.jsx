0e535fcf3d2aed4a7684517dae3d97cabc530348
akliyachir <akli.yachir@gmail.com>
2023-02-07 04:31:44 -0300

    mern stack, article publishing platform, currently getting lexical to work

--- articles_publishing_platform_v1/client/src/lexical_rich_text_config/src/Editor.jsx
+++ articles_publishing_platform_v1/client/src/lexical_rich_text_config/src/Editor.jsx
@@ -1,3 +1,11 @@
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
+
+import { $getRoot, $getSelection } from 'lexical';
+import { useEffect } from 'react';
+
+import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+
 import ExampleTheme from './themes/ExampleTheme';
 import { LexicalComposer } from '@lexical/react/LexicalComposer';
 import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
@@ -20,35 +28,75 @@ import { TRANSFORMERS } from '@lexical/markdown';
 import ListMaxIndentLevelPlugin from './plugins/ListMaxIndentLevelPlugin';
 import CodeHighlightPlugin from './plugins/CodeHighlightPlugin';
 import AutoLinkPlugin from './plugins/AutoLinkPlugin';
+import { useState } from 'react';
 
 function Placeholder() {
   return <div className='editor-placeholder'>Enter some rich text...</div>;
 }
 
-const editorConfig = {
-  // The editor theme
-  theme: ExampleTheme,
-  // Handling of errors during update
-  onError(error) {
-    throw error;
-  },
-  // Any custom nodes go here
-  nodes: [
-    HeadingNode,
-    ListNode,
-    ListItemNode,
-    QuoteNode,
-    CodeNode,
-    CodeHighlightNode,
-    TableNode,
-    TableCellNode,
-    TableRowNode,
-    AutoLinkNode,
-    LinkNode,
-  ],
-};
-
 export default function Editor() {
+  const [editorContent, setEditorContent] = useState('');
+
+  function onChange(editorState) {
+    editorState.read(() => {
+      // Read the contents of the EditorState here.
+      const root = $getRoot();
+      const selection = $getSelection();
+
+      console.log(root, selection);
+    });
+  }
+
+  const editorConfig = {
+    // The editor theme
+    theme: ExampleTheme,
+    // Handling of errors during update
+    onError(error) {
+      throw error;
+    },
+    // Any custom nodes go here
+    nodes: [
+      HeadingNode,
+      ListNode,
+      ListItemNode,
+      QuoteNode,
+      CodeNode,
+      CodeHighlightNode,
+      TableNode,
+      TableCellNode,
+      TableRowNode,
+      AutoLinkNode,
+      LinkNode,
+    ],
+  };
+
+  function onError(error) {
+    console.error(error);
+  }
+
+  function Editor() {
+    const initialConfig = {
+      namespace: 'MyEditor',
+      theme,
+      onError,
+    };
+  }
+
+  function onError(error) {
+    console.error(error);
+  }
+
+  function MyCustomAutoFocusPlugin() {
+    const [editor] = useLexicalComposerContext();
+
+    useEffect(() => {
+      // Focus the editor when the effect fires!
+      editor.focus();
+    }, [editor]);
+
+    return null;
+  }
+
   return (
     <LexicalComposer initialConfig={editorConfig}>
       <div className='editor-container'>
@@ -63,12 +111,25 @@ export default function Editor() {
           {/* <TreeViewPlugin /> */}
           <AutoFocusPlugin />
           <CodeHighlightPlugin />
+          <OnChangePlugin onChange={onChange} />
           <ListPlugin />
           <LinkPlugin />
           <AutoLinkPlugin />
           <ListMaxIndentLevelPlugin maxDepth={7} />
           <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
         </div>
+        <LexicalComposer initialConfig={initialConfig}>
+          <PlainTextPlugin
+            contentEditable={<ContentEditable />}
+            placeholder={<div>Enter some text...</div>}
+            ErrorBoundary={LexicalErrorBoundary}
+          />
+          <HistoryPlugin />
+          <MyCustomAutoFocusPlugin />
+        </LexicalComposer>
+        {editorContent}
+
+        <div></div>
       </div>
     </LexicalComposer>
   );
