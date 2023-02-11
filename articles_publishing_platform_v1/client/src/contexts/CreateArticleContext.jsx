2a3334c7a78d00f6ef202b44a26f76e953b66200
akliyachir <akli.yachir@gmail.com>
2023-02-11 07:10:13 -0300

    creating use context to manage creating article and after the modify article

--- articles_publishing_platform_v1/client/src/contexts/CreateArticleContext.jsx
+++ articles_publishing_platform_v1/client/src/contexts/CreateArticleContext.jsx
@@ -1,9 +1,26 @@
 import { createContext, useReducer } from 'react'
 
+const CreateArticleReducer = (CreateArticleState, action) => {
+  switch (action.type) {
+  }
+}
+const CreateArticleDefaultValue = {
+  article_title: '',
+  article_image_url: '',
+  article_image_height: 50,
+  article_body: '',
+  article_body_shorten_for_card: '',
+  article_id: crypto.randomUUID(),
+  article_is_public: true,
+}
+
 export const CreateArticleContext = createContext(null)
-const [createArticleFormData, setCreateArticleFormData] = useState({})
 
 export default function CreateArticleContextProvider({ children }) {
+  const [CreateArticleState, CreateArticleDispatch] = useReducer(
+    CreateArticleReducer,
+    CreateArticleDefaultValue
+  )
   return (
     <CreateArticleContextProvider.Provider value={'on verra'}>
       {children}
