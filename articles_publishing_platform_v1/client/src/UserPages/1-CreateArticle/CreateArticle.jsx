2a01d3ceea1024c0ceb6b625dd0ee5aaa50eae36
akliyachir <akli.yachir@gmail.com>
2023-02-11 06:09:01 -0300

    debugging the the reason why an article need to be submitted two times to be sent, now whene i write in the text editor, the title and the picture desapears, creating a context to manage my buggy prop drilling as a last resort, and now it's a mess, its brutal, now switching to a global useContext

--- articles_publishing_platform_v1/client/src/UserPages/1-CreateArticle/CreateArticle.jsx
+++ articles_publishing_platform_v1/client/src/UserPages/1-CreateArticle/CreateArticle.jsx
@@ -1,15 +1,3 @@
-import { EditorContent, useEditor } from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
-import {
-  GrBold,
-  GrItalic,
-  GrTextAlignFull,
-  GrOrderedList,
-  GrUnorderedList,
-  GrRedo,
-  GrUndo,
-} from 'react-icons/gr';
-
 import './CreateArticle.css';
 import {
   useState,
@@ -21,7 +9,7 @@ import {
 } from 'react';
 import { useNavigate } from 'react-router-dom';
 import backendUrl from '../../listsAndReusedConsts/backendUrl';
-
+import TiptapRichTextEditor from '../../TiptapRichTextEditor/TiptapRichTextEditor';
 // -- create a context, my last resort :'(
 export const PreviewsValues = createContext({});
 
@@ -198,20 +186,6 @@ export default function CreateArticle() {
               />
             </div>
           )}
-          <div className='TiptapRichTextEditor'>
-            <div className='TiptapRichTextEditorContent'>
-              <TipTapEditor
-                setCreateArticleFormData={setCreateArticleFormData}
-                setarticleLengthCheck={setarticleLengthCheck}
-              />
-              {articleLengthCheck.length > 8000 && (
-                <div className='bodyTextEditorErrorMessage'>
-                  <p>Too much content</p>
-                  <p>Exceeding 8000 characters!</p>
-                </div>
-              )}
-            </div>
-          </div>
           <div
             className={
               articleLengthCheck && articleLengthCheck.length > 8000
@@ -219,6 +193,23 @@ export default function CreateArticle() {
                 : 'NoErrorTextEditor'
             }
           >
+            <PreviewsValues.Provider
+              value={{
+                article_title,
+                article_image_url,
+                article_body,
+                article_body_shorten_for_card,
+                article_image_height,
+                createArticleFormData,
+              }}
+            >
+              <TiptapRichTextEditor
+                setCreateArticleFormData={setCreateArticleFormData}
+                setarticleLengthCheck={setarticleLengthCheck}
+                articleLengthCheck={articleLengthCheck}
+              />
+            </PreviewsValues.Provider>
+
             {!!atLeast300CharactersMessage && (
               <div className='ThreeHundredCharactersMEssageArea'>
                 {atLeast300CharactersMessage}
@@ -237,151 +228,3 @@ export default function CreateArticle() {
     </div>
   );
 }
-//--
-
-import { RiH1, RiH2, RiTextWrap } from 'react-icons/ri';
-
-const MenuBar = ({ editor }) => {
-  if (!editor) {
-    return null;
-  }
-
-  return (
-    <div className='TextEditorMenu'>
-      <div className='TextEditorMenuContent'>
-        <div className='sectionFirst'>
-          <button
-            onClick={() => editor.chain().focus().setParagraph().run()}
-            className={editor.isActive('paragraph') ? 'is-active' : ''}
-          >
-            <div className='TipTapIconsStyling'>
-              <GrTextAlignFull />
-            </div>
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleBold().run()}
-            disabled={!editor.can().chain().focus().toggleBold().run()}
-            className={editor.isActive('bold') ? 'is-active' : ''}
-          >
-            <div className='TipTapIconsStyling'>
-              <GrBold />
-            </div>
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleItalic().run()}
-            disabled={!editor.can().chain().focus().toggleItalic().run()}
-            className={editor.isActive('italic') ? 'is-active' : ''}
-          >
-            <div className='TipTapIconsStyling'>
-              <GrItalic />
-            </div>
-          </button>
-          <button
-            onClick={() =>
-              editor.chain().focus().toggleHeading({ level: 2 }).run()
-            }
-            className={
-              editor.isActive('heading', { level: 2 }) ? 'is-active' : ''
-            }
-          >
-            <div className='TipTapIconsStyling'>
-              <RiH1 />
-            </div>
-          </button>
-          <button
-            onClick={() =>
-              editor.chain().focus().toggleHeading({ level: 3 }).run()
-            }
-            className={
-              editor.isActive('heading', { level: 3 }) ? 'is-active' : ''
-            }
-          >
-            <div className='TipTapIconsStyling'>
-              <RiH2 />
-            </div>
-          </button>
-        </div>
-        <div className='sectionSecond'>
-          <button
-            onClick={() => editor.chain().focus().toggleBulletList().run()}
-            className={editor.isActive('bulletList') ? 'is-active' : ''}
-          >
-            <div className='TipTapIconsStyling'>
-              <GrUnorderedList />
-            </div>
-          </button>
-          <button
-            onClick={() => editor.chain().focus().toggleOrderedList().run()}
-            className={editor.isActive('orderedList') ? 'is-active' : ''}
-          >
-            <div className='TipTapIconsStyling'>
-              <GrOrderedList />
-            </div>
-          </button>
-
-          <button onClick={() => editor.chain().focus().setHardBreak().run()}>
-            <div className='TipTapIconsStyling'>
-              <RiTextWrap />
-            </div>
-          </button>
-          <button
-            onClick={() => editor.chain().focus().undo().run()}
-            disabled={!editor.can().chain().focus().undo().run()}
-          >
-            <div className='TipTapIconsStyling'>
-              <GrUndo />
-            </div>
-          </button>
-          <button
-            onClick={() => editor.chain().focus().redo().run()}
-            disabled={!editor.can().chain().focus().redo().run()}
-          >
-            <div className='TipTapIconsStyling'>
-              <GrRedo />
-            </div>
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const TipTapEditor = ({ setarticleLengthCheck, setCreateArticleFormData }) => {
-  const {
-    article_title,
-    article_image_url,
-    article_body,
-    article_body_shorten_for_card,
-    article_image_height,
-    createArticleFormData,
-  } = useContext(PreviewsValues);
-
-  const editor = useEditor({
-    extensions: [StarterKit],
-    content: '',
-    onUpdate: ({ editor }) => {
-      const html = editor.getHTML();
-      const plainText = editor.getText().replace(/['\n']/gi, ' ');
-      const plainTextShorten = plainText.slice(0, 180);
-
-      setCreateArticleFormData({
-        article_title,
-        article_image_url,
-        article_image_height,
-        createArticleFormData,
-        article_body: html,
-        article_body_shorten_for_card: plainTextShorten,
-        ...createArticleFormData,
-      });
-      console.log(createArticleFormData);
-      setarticleLengthCheck(plainText);
-    },
-  });
-
-  return (
-    <div>
-      <MenuBar editor={editor} />
-      <EditorContent editor={editor} />
-    </div>
-  );
-};
--- articles_publishing_platform_v1/client/src/contexts/userContext.jsx
+++ articles_publishing_platform_v1/client/src/contexts/userContext.jsx
@@ -4,12 +4,35 @@ import { userReducer, userDefaultState } from '../reducers/userReducer';
 export const UserContext = createContext();
 
 export default function UserContextProvider({ children }) {
-	// -- useReducer
-	const [userState, userDispatch] = useReducer(userReducer, userDefaultState);
-	// -- return component
-	return (
-		<UserContext.Provider value={{ userState, userDispatch }}>
-			{children}
-		</UserContext.Provider>
-	);
+  // -- useReducer
+  const [userState, userDispatch] = useReducer(userReducer, userDefaultState);
+  // -- return component
+  return (
+    <UserContext.Provider value={{ userState, userDispatch }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
+
+/* 
+
+
+  const [createArticleFormData, setCreateArticleFormData] = useState({
+    article_title: '',
+    article_image_url: '',
+    article_image_height: 50,
+    article_body: '',
+    article_body_shorten_for_card: '',
+    article_id: crypto.randomUUID(),
+    article_is_public: true,
+  });
+  const {
+    article_title,
+    article_image_url,
+    article_body,
+    article_body_shorten_for_card,
+    article_image_height,
+  } = createArticleFormData;
+
+
+*/
