c16f2ae3375964b46b767c4ac365d4ab299381a3
akliyachir <akli.yachir@gmail.com>
2023-02-13 00:27:17 -0300

    adding update article to the platform

--- articles_publishing_platform_v1/client/src/App.jsx
+++ articles_publishing_platform_v1/client/src/App.jsx
@@ -1,83 +1,89 @@
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom'
 // -- pages import
-import RootLayout from './Pages/1-RootLayout/RootLayout';
-import ErrorPage from './Pages/2-ErrorPage/ErrorPage';
-import SignUpContainer from './Pages/4-SignUp/SignUpContainer';
-import SignInContainer from './Pages/3-SignIn/SignInContainer';
+import RootLayout from './Pages/1-RootLayout/RootLayout'
+import ErrorPage from './Pages/2-ErrorPage/ErrorPage'
+import SignUpContainer from './Pages/4-SignUp/SignUpContainer'
+import SignInContainer from './Pages/3-SignIn/SignInContainer'
 // -- contexts
-import UserContextProvider from './Contexts/UserContext';
-import CreateArticle from './UserPages/1-CreateArticle/CreateArticle';
-import GetUserArticles from './UserPages/2-GetUserArticles/GetUserArticles';
-import TestComponent from './TestComponent/TestComponent';
-import GetUserFullArticleTemplate from './UserPages/3-GetUserFullArticleTemplate/GetUserFullArticleTemplate';
-import ShowAllPublicArticles from './Pages/5-ShowAllPublicArticles/ShowAllPublicArticles';
-import FullPublicArticle from './TemplatePages/2-FullPublicArticle/FullPublicArticle';
-import UserDisconnect from './UserPages/0-UserDisconnect/UserDisconnect';
+import UserContextProvider from './Contexts/UserContext'
+import CreateArticle from './UserPages/1-CreateArticle/CreateArticle'
+import GetUserArticles from './UserPages/2-GetUserArticles/GetUserArticles'
+import TestComponent from './TestComponent/TestComponent'
+import GetUserFullArticleTemplate from './UserPages/3-GetUserFullArticleTemplate/GetUserFullArticleTemplate'
+import ShowAllPublicArticles from './Pages/5-ShowAllPublicArticles/ShowAllPublicArticles'
+import FullPublicArticle from './TemplatePages/2-FullPublicArticle/FullPublicArticle'
+import UserDisconnect from './UserPages/0-UserDisconnect/UserDisconnect'
+import UpdateUserArticle from './UserPages/4-UpdateUserArticle/UpdateUserArticle'
 
 // -- user routes import
 //-- create router
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<RootLayout />} errorElement={<ErrorPage />}>
-      <Route
-        path='/'
-        element={<ShowAllPublicArticles />}
-        errorElement={<ErrorPage />}
-      />
-      <Route
-        path='/article/:public_article_id'
-        element={<FullPublicArticle />}
-        errorElement={<ErrorPage />}
-      />
-      <Route
-        path='/signin'
-        element={<SignInContainer />}
-        errorElement={<ErrorPage />}
-      />
-      <Route
-        path='/signup'
-        element={<SignUpContainer />}
-        errorElement={<ErrorPage />}
-      />
-      <Route
-        path='/disconnect'
-        element={<UserDisconnect />}
-        errorElement={<ErrorPage />}
-      />
-      <Route
-        path='/create_article'
-        element={<CreateArticle />}
-        errorElement={<ErrorPage />}
-      />
-      <Route
-        path='/user_articles'
-        element={<GetUserArticles />}
-        errorElement={<ErrorPage />}
-      />
-      <Route
-        path='/user_article/:user_article_id'
-        element={<GetUserFullArticleTemplate />}
-        errorElement={<ErrorPage />}
-      />
+	createRoutesFromElements(
+		<Route path='/' element={<RootLayout />} errorElement={<ErrorPage />}>
+			<Route
+				path='/'
+				element={<ShowAllPublicArticles />}
+				errorElement={<ErrorPage />}
+			/>
+			<Route
+				path='/article/:public_article_id'
+				element={<FullPublicArticle />}
+				errorElement={<ErrorPage />}
+			/>
+			<Route
+				path='/signin'
+				element={<SignInContainer />}
+				errorElement={<ErrorPage />}
+			/>
+			<Route
+				path='/signup'
+				element={<SignUpContainer />}
+				errorElement={<ErrorPage />}
+			/>
+			<Route
+				path='/disconnect'
+				element={<UserDisconnect />}
+				errorElement={<ErrorPage />}
+			/>
+			<Route
+				path='/create_article'
+				element={<CreateArticle />}
+				errorElement={<ErrorPage />}
+			/>
+			<Route
+				path='/user_articles'
+				element={<GetUserArticles />}
+				errorElement={<ErrorPage />}
+			/>
+			<Route
+				path='/update_article:article_update_id'
+				element={<UpdateUserArticle />}
+				errorElement={<ErrorPage />}
+			/>
+			<Route
+				path='/user_article/:user_article_id'
+				element={<GetUserFullArticleTemplate />}
+				errorElement={<ErrorPage />}
+			/>
 
-      <Route
-        path='/tomato'
-        element={<TestComponent />}
-        errorElement={<ErrorPage />}
-      />
-    </Route>
-  )
-);
+			<Route
+				path='/tomato'
+				element={<TestComponent />}
+				errorElement={<ErrorPage />}
+			/>
+		</Route>
+	)
+)
 
 export default function App() {
-  return (
-    <UserContextProvider>
-      <RouterProvider router={router} />
-    </UserContextProvider>
-  );
+	return (
+		<UserContextProvider>
+			<RouterProvider router={router} />
+		</UserContextProvider>
+	)
 }
---
+++ articles_publishing_platform_v1/client/src/UserPages/4-UpdateUserArticle/UpdateUserArticle.jsx
@@ -0,0 +1,12 @@
+import { useParams } from 'react-router-dom'
+
+export default function UpdateUserArticle() {
+	const { article_update_id } = useParams()
+	return (
+		<div className='UpdateUserArticle'>
+			<div className='UpdateUserArticleContent'>
+				Update Article <span>{article_update_id}</span>
+			</div>
+		</div>
+	)
+}
