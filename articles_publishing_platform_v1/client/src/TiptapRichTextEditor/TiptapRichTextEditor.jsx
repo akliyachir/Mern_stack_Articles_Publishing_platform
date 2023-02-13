ee615821d703b04f5d81e73fe9c9a703f26c56f6
akliyachir <akli.yachir@gmail.com>
2023-02-13 20:26:43 -0300

    back to working on the article publishing platform, starting with cleaning my last mess

--- articles_publishing_platform_v1/client/src/TiptapRichTextEditor/TiptapRichTextEditor.jsx
+++ articles_publishing_platform_v1/client/src/TiptapRichTextEditor/TiptapRichTextEditor.jsx
@@ -8,27 +8,22 @@ export default function TiptapRichTextEditor({
 	setarticleLengthCheck,
 	articleLengthCheck,
 }) {
-	useEffect(() => {
-		setisMounted(true);
-	}, []);
 	return (
-		isMounted && (
-			<div className='TiptapRichTextEditor'>
-				<div className='TiptapRichTextEditorContent'>
-					<TipTapEditor
-						getContentFromTextEditor={getContentFromTextEditor}
-						setGetContentFromTextEditor={setGetContentFromTextEditor}
-						setarticleLengthCheck={setarticleLengthCheck}
-						articleLengthCheck={articleLengthCheck}
-					/>
-					{articleLengthCheck.length > 8000 && (
-						<div className='bodyTextEditorErrorMessage'>
-							<p>Too much content</p>
-							<p>Exceeding 8000 characters!</p>
-						</div>
-					)}
-				</div>
+		<div className='TiptapRichTextEditor'>
+			<div className='TiptapRichTextEditorContent'>
+				<TipTapEditor
+					getContentFromTextEditor={getContentFromTextEditor}
+					setGetContentFromTextEditor={setGetContentFromTextEditor}
+					setarticleLengthCheck={setarticleLengthCheck}
+					articleLengthCheck={articleLengthCheck}
+				/>
+				{articleLengthCheck.length > 8000 && (
+					<div className='bodyTextEditorErrorMessage'>
+						<p>Too much content</p>
+						<p>Exceeding 8000 characters!</p>
+					</div>
+				)}
 			</div>
-		)
+		</div>
 	);
 }
