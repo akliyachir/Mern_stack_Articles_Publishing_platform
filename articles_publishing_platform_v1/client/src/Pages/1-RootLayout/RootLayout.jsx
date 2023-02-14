ccbc98118512e6c228e93f81cacd2e78c8246bf1
akliyachir <akli.yachir@gmail.com>
2023-02-14 05:42:56 -0300

    starting to cloturate the platform devloppment, making the website little more pretty

--- articles_publishing_platform_v1/client/src/Pages/1-RootLayout/RootLayout.jsx
+++ articles_publishing_platform_v1/client/src/Pages/1-RootLayout/RootLayout.jsx
@@ -1,33 +1,35 @@
-import './RootLayout.css'
-import { useContext, useEffect } from 'react'
-import { Outlet } from 'react-router-dom'
-import HeaderContainer from '../../Components/1-HeaderContainer/HeaderContainer'
-import FooterHomePage from '../../Components/5-FooterHomePage/FooterHomePage'
-import { UserContext } from '../../Contexts/UserContext'
-import CreateArticleContextProvider from '../../contexts/CreateArticleContext'
+import './RootLayout.css';
+import { useContext, useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import HeaderContainer from '../../Components/1-HeaderContainer/HeaderContainer';
+import FooterHomePage from '../../Components/5-FooterHomePage/FooterHomePage';
+import { UserContext } from '../../Contexts/UserContext';
 
 export default function RootLayout() {
-	const { userState, userDispatch } = useContext(UserContext)
-
+	const { userState, userDispatch } = useContext(UserContext);
+	const [WebsiteTitleDesapearing, setWebsiteTitleDesapearing] = useState(false);
 	useEffect(() => {
-		const userFromLocalStorageAsJson = globalThis.localStorage.getItem('user')
+		const userFromLocalStorageAsJson = globalThis.localStorage.getItem('user');
 		if (userFromLocalStorageAsJson) {
-			const userFromLocalStorage = JSON.parse(userFromLocalStorageAsJson)
-			const { name, email, token } = userFromLocalStorage
-			userDispatch({ type: 'SIGN_IN', payload: { name, email, token } })
+			const userFromLocalStorage = JSON.parse(userFromLocalStorageAsJson);
+			const { name, email, token } = userFromLocalStorage;
+			userDispatch({ type: 'SIGN_IN', payload: { name, email, token } });
 		}
-	}, [])
+	}, []);
 	return (
 		<div className='RootLayout'>
 			<div className='RootLayoutContent'>
 				<nav>
 					<HeaderContainer />
 				</nav>
+				<h1 className={false ? 'RootWebsiteTitleClosed' : 'RootWebsiteTitle'}>
+					The best way to publish your Article
+				</h1>
 				<main className='mainAllOutputedArticles'>
 					<Outlet />
 				</main>
 				<FooterHomePage />
 			</div>
 		</div>
-	)
+	);
 }
