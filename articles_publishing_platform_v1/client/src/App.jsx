9da25fcff7c29b07179ed467d0da85b4da532076
akliyachir <akli.yachir@gmail.com>
2023-02-14 04:18:43 -0300

    starting to cloturate the platform devloppment

--- articles_publishing_platform_v1/client/src/App.jsx
+++ articles_publishing_platform_v1/client/src/App.jsx
@@ -3,22 +3,21 @@ import {
 	createRoutesFromElements,
 	Route,
 	RouterProvider,
-} from 'react-router-dom'
+} from 'react-router-dom';
 // -- pages import
-import RootLayout from './Pages/1-RootLayout/RootLayout'
-import ErrorPage from './Pages/2-ErrorPage/ErrorPage'
-import SignUpContainer from './Pages/4-SignUp/SignUpContainer'
-import SignInContainer from './Pages/3-SignIn/SignInContainer'
+import RootLayout from './Pages/1-RootLayout/RootLayout';
+import ErrorPage from './Pages/2-ErrorPage/ErrorPage';
+import SignUpContainer from './Pages/4-SignUp/SignUpContainer';
+import SignInContainer from './Pages/3-SignIn/SignInContainer';
 // -- contexts
-import UserContextProvider from './Contexts/UserContext'
-import CreateArticle from './UserPages/1-CreateArticle/CreateArticle'
-import GetUserArticles from './UserPages/2-GetUserArticles/GetUserArticles'
-import TestComponent from './TestComponent/TestComponent'
-import GetUserFullArticleTemplate from './UserPages/3-GetUserFullArticleTemplate/GetUserFullArticleTemplate'
-import ShowAllPublicArticles from './Pages/5-ShowAllPublicArticles/ShowAllPublicArticles'
-import FullPublicArticle from './TemplatePages/2-FullPublicArticle/FullPublicArticle'
-import UserDisconnect from './UserPages/0-UserDisconnect/UserDisconnect'
-import UpdateUserArticle from './UserPages/4-UpdateUserArticle/UpdateUserArticle'
+import UserContextProvider from './Contexts/UserContext';
+import CreateArticle from './UserPages/1-CreateArticle/CreateArticle';
+import GetUserArticles from './UserPages/2-GetUserArticles/GetUserArticles';
+import TestComponent from './TestComponent/TestComponent';
+import GetUserFullArticleTemplate from './UserPages/3-GetUserFullArticleTemplate/GetUserFullArticleTemplate';
+import ShowAllPublicArticles from './Pages/5-ShowAllPublicArticles/ShowAllPublicArticles';
+import FullPublicArticle from './TemplatePages/2-FullPublicArticle/FullPublicArticle';
+import UserDisconnect from './UserPages/0-UserDisconnect/UserDisconnect';
 
 // -- user routes import
 //-- create router
@@ -61,11 +60,6 @@ const router = createBrowserRouter(
 				errorElement={<ErrorPage />}
 			/>
 			<Route
-				path='/user_article_update/:article_update_id'
-				element={<UpdateUserArticle />}
-				errorElement={<ErrorPage />}
-			/>
-			<Route
 				path='/user_article/:user_article_id'
 				element={<GetUserFullArticleTemplate />}
 				errorElement={<ErrorPage />}
@@ -78,12 +72,12 @@ const router = createBrowserRouter(
 			/>
 		</Route>
 	)
-)
+);
 
 export default function App() {
 	return (
 		<UserContextProvider>
 			<RouterProvider router={router} />
 		</UserContextProvider>
-	)
+	);
 }
